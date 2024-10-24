74996d42ff4758ba1c9eac30d1ac8efdeb52ac1d
benreasinger <reasingerben@gmail.com>
2024-10-24 16:23:31 -0400

    Add files via upload

---
+++ Scripts/musicScript.js
@@ -0,0 +1,429 @@
+let albums = [
+//   Pop-Rock
+    {
+    album: "Spirits Having Flown",
+    release_date: 1979,
+    Artist : "BeeGees",
+    Genre : "Pop-Rock",
+    Artwork: `../assets/SHF.jpg`,
+    Rating: 3
+   },
+   {
+    album: "High Civilization",
+    release_date: 1991,
+    Artist : "BeeGees",
+    Genre : "Pop-Rock",
+    Artwork: "../assets/HC.jpg",
+    Rating: 2.3
+   },
+   {
+    album: "Children Of the World",
+    release_date: 1976,
+    Artist : "BeeGees",
+    Genre : "Pop-Rock",
+    Artwork: "../assets/COTW.jpg",
+    Rating: 4.7
+   },
+   {
+    album: "Saturday Night Fever",
+    release_date: 1977,
+    Artist : "BeeGees",
+    Genre : "Pop-Rock",
+    Artwork: "../assets/SNF.jpg",
+    Rating: 5
+   },
+   {
+    album: "Bee Gees'1st",
+    release_date: 1967,
+    Artist : "BeeGees",
+    Genre : "Pop-Rock",
+    Artwork: "../assets/BG1.jpg",
+    Rating: 3
+   },
+
+// Pop-Rock
+{
+  album: "Spirits Having Flown",
+  release_date: 1979,
+  Artist: "Bee Gees",
+  Genre: "Pop-Rock",
+  Rating: 3
+},
+// Alternative Rock
+{
+  album: "OK Computer",
+  release_date: 1997,
+  Artist: "Radiohead",
+  Genre: "Alternative Rock",
+  Rating: 5
+},
+// Hip-Hop
+{
+  album: "To Pimp a Butterfly",
+  release_date: 2015,
+  Artist: "Kendrick Lamar",
+  Genre: "Hip-Hop",
+  Rating: 5
+},
+// Pop
+{
+  album: "Future Nostalgia",
+  release_date: 2020,
+  Artist: "Dua Lipa",
+  Genre: "Pop",
+  Rating: 4
+},
+// R&B
+{
+  album: "Channel Orange",
+  release_date: 2012,
+  Artist: "Frank Ocean",
+  Genre: "R&B",
+  Rating: 5
+},
+// Jazz
+{
+  album: "Kind of Blue",
+  release_date: 1959,
+  Artist: "Miles Davis",
+  Genre: "Jazz",
+  Rating: 5
+},
+// Heavy Metal
+{
+  album: "Master of Puppets",
+  release_date: 1986,
+  Artist: "Metallica",
+  Genre: "Heavy Metal",
+  Rating: 5
+},
+// Electronic
+{
+  album: "Discovery",
+  release_date: 2001,
+  Artist: "Daft Punk",
+  Genre: "Electronic",
+  Rating: 5
+},
+// Classic Rock
+{
+  album: "Rumours",
+  release_date: 1977,
+  Artist: "Fleetwood Mac",
+  Genre: "Classic Rock",
+  Rating: 5
+},
+// Soul
+{
+  album: "Let's Get It On",
+  release_date: 1973,
+  Artist: "Marvin Gaye",
+  Genre: "Soul",
+  Rating: 4
+},
+// Funk
+{
+  album: "Songs in the Key of Life",
+  release_date: 1976,
+  Artist: "Stevie Wonder",
+  Genre: "Funk",
+  Rating: 5
+},
+// Grunge
+{
+  album: "Nevermind",
+  release_date: 1991,
+  Artist: "Nirvana",
+  Genre: "Grunge",
+  Rating: 5
+},
+// Indie Pop
+{
+  album: "Melodrama",
+  release_date: 2017,
+  Artist: "Lorde",
+  Genre: "Indie Pop",
+  Rating: 4
+},
+// Reggae
+{
+  album: "Legend",
+  release_date: 1984,
+  Artist: "Bob Marley and the Wailers",
+  Genre: "Reggae",
+  Rating: 5
+},
+// Punk Rock
+{
+  album: "Dookie",
+  release_date: 1994,
+  Artist: "Green Day",
+  Genre: "Punk Rock",
+  Rating: 4
+},
+// Blues Rock
+{
+  album: "The Sky Is Crying",
+  release_date: 1991,
+  Artist: "Stevie Ray Vaughan",
+  Genre: "Blues Rock",
+  Rating: 5
+},
+// Country
+{
+  album: "Golden Hour",
+  release_date: 2018,
+  Artist: "Kacey Musgraves",
+  Genre: "Country",
+  Rating: 4
+},
+// Classical
+{
+  album: "The Four Seasons",
+  release_date: 1725,
+  Artist: "Antonio Vivaldi",
+  Genre: "Classical",
+  Rating: 5
+},
+// Folk Rock
+{
+  album: "Harvest",
+  release_date: 1972,
+  Artist: "Neil Young",
+  Genre: "Folk Rock",
+  Rating: 5
+},
+// Synthpop
+{
+  album: "Violator",
+  release_date: 1990,
+  Artist: "Depeche Mode",
+  Genre: "Synthpop",
+  Rating: 4
+},
+// Progressive Rock
+{
+  album: "The Dark Side of the Moon",
+  release_date: 1973,
+  Artist: "Pink Floyd",
+  Genre: "Progressive Rock",
+  Rating: 5
+},
+// Hard Rock
+{
+  album: "Back in Black",
+  release_date: 1980,
+  Artist: "AC/DC",
+  Genre: "Hard Rock",
+  Rating: 5
+},
+// Pop Punk
+{
+  album: "Enema of the State",
+  release_date: 1999,
+  Artist: "Blink-182",
+  Genre: "Pop Punk",
+  Rating: 4
+},
+// Neo-Soul
+{
+  album: "Voodoo",
+  release_date: 2000,
+  Artist: "D'Angelo",
+  Genre: "Neo-Soul",
+  Rating: 5
+},
+// Psychedelic Rock
+{
+  album: "Electric Ladyland",
+  release_date: 1968,
+  Artist: "The Jimi Hendrix Experience",
+  Genre: "Psychedelic Rock",
+  Rating: 5
+},
+
+
+
+
+//    RNB / RAP
+   {
+    album: "It Takes A Nation Of Millions To Hold Us Back",
+    release_date: 1988,
+    Artist : "Public Enemy",
+    Genre : "RNB / RAP",
+    Artwork: "../assets/itan.webp",
+    Rating: 2.6
+   },
+   {
+    album: "Paid In Full",
+    release_date: 1987,
+    Artist : "Eric B & Rakim",
+    Genre : "RNB / RAP",
+    Artwork: "../assets/PIF.jpg",
+    Rating: 3
+   },
+   {
+    album: "Savage Mode II",
+    release_date: 2020,
+    Artist : "21 Savage & Metro Boomin",
+    Genre : "RNB / RAP",
+    Artwork: "../assets/SM2.jpg",
+    Rating: 4.3
+   },
+   {
+    album: "american dream",
+    release_date: 2024,
+    Artist : "21 Savage",
+    Genre : "RNB / RAP",
+    Artwork: "../assets/AMD.jpg",
+    Rating: 3.5
+   },
+   {
+    album: "The Smartest",
+    release_date: 2020,
+    Artist : "Tee Grizzley",
+    Genre : "RNB / RAP",
+    Artwork: "../assets/TS.jpg",
+    Rating: 2
+   },
+
+
+
+
+//   Country
+   {
+    album: "Zach Bryan",
+    release_date: 2023,
+    Artist : "Zach Bryan",
+    Genre : "Country",
+    Artwork: "../assets/zb.webp",
+    Rating: 4.6
+   },
+   {
+    album: "The Great American Bar Scene",
+    release_date: 2024,
+    Artist : "Zach Bryan",
+    Genre : "Country",
+    Artwork: "../assets/TGA.jpg",
+    Rating: 3.4
+   },
+   {
+    album: "Million Eyes",
+    release_date: 2023,
+    Artist : "Sam Barber",
+    Genre : "Country",
+    Artwork: "../assets/ME.webp",
+    Rating: 3.7
+   },
+   {
+    album: "One Thing At A Time",
+    release_date: 2023,
+    Artist : "Morgan Wallen",
+    Genre : "Country",
+    Artwork: "../assets/ot.webp",
+    Rating: 1.7
+   },
+   {
+    album: "Religiously. The Album.",
+    release_date: 2023,
+    Artist : "Bailey Zimmerman",
+    Genre : "Country",
+    Artwork: "../assets/RTA.jpg",
+    Rating: 3.6
+   },
+   
+
+
+
+
+//    Folk Rock
+    {
+    album: "Cleopatra",
+    release_date: 2016,
+    Artist : "The Lumineers",
+    Genre : "Folk-Rock",
+    Artwork: "../assets/CP.jpg",
+    Rating: 4.6
+   },
+   {
+    album: "Brightside",
+    release_date: 2022,
+    Artist : "The Lumineers",
+    Genre : "Folk-Rock",
+    Artwork: "../assets/BS.webp",
+    Rating: 3
+   },
+   {
+    album: "The Lumineers",
+    release_date: 2012,
+    Artist : "The Lumineers",
+    Genre : "Folk-Rock",
+    Artwork: "../assets/TL.webp",
+    Rating: 4.3
+   },
+   {
+    album: "III",
+    release_date: 2019,
+    Artist : "The Lumineers",
+    Genre : "Folk-Rock",
+    Artwork: "../assets/III.webp",
+    Rating: 2.7
+   },
+   {
+    album: "Bringing It All Back Home",
+    release_date: 1965,
+    Artist: "The Lumineers",
+    Genre: "Folk-Rock",
+    Artwork: "../assets/BIA.webp",
+    Rating: 5
+   },
+];
+
+
+
+albums.forEach(function(albums){
+    let template =
+     `
+      <div class="child">
+      <h1 class="name">${albums.Genre}<br></h1>
+        <h1 class="name">${albums.album}</h1>
+        <h2>${albums.Artist}</h2>
+        <h3>${albums.release_date}</h3>
+      <img src='${albums.Artwork}' width="75px" height="75"></img>
+        <h3>${albums.Rating}</h3>
+      </div>
+    `
+    document.querySelector(".container").innerHTML += template;
+  })
+
+
+
+  function filterartist(Genre) {
+  
+    document.querySelector(".container").innerHTML = "";
+   console.log("test")
+let filtAlbums = albums.filter(function(filtAlbums){
+return filtAlbums.Genre == Genre;});
+filtAlbums.forEach(function(albums){
+  let template = 
+   `
+    <div class="child">
+    <h1 class="name">${albums.Genre}<br></h1>:
+      <h1 class="name">${albums.album}</h1>
+      <h2>${albums.Artist}</h2>
+      <h3>${albums.release_date}</h3>
+      <img src='${albums.Artwork}' width="75px" height="75"></img>
+      <h3>${albums.Rating}</h3>
+    </div>
+  `
+  document.querySelector(".container").innerHTML += template;
+})
+};
+
+
+
+
+
+function home() {
+  location.reload();
+}
