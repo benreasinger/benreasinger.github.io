a66c7f4849e1f65df9b2f7c2c3dbd6cd84804e3a
benreasinger <reasingerben@gmail.com>
2024-10-24 16:34:28 -0400

    Update script.js

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,6 +1,6 @@
 function toggleImage() {
     const image = document.getElementById("toggle-img");
-    if (image.src.includes("Photo1.jpg")) {
+    if (image.src.includes("Photo1.JPG")) {
         image.src = "../assets/Photo2.png";
     }
      else if(image.src.includes("Photo2.png"))  {
@@ -13,6 +13,6 @@ function toggleImage() {
         image.src = "../assets/Photo5.jpg";
      }
      else if(image.src.includes("Photo5.jpg")){
-        image.src = "../assets/Photo1.jpg";
+        image.src = "../assets/Photo1.JPG";
      }
 }
